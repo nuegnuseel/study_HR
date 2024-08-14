18753ada3364c4db1475118e5dac010a043f03d1
lee0502 <i63774071@gmail.com>
2024-08-14 10:50:09 +0900

    first commit

---
+++ src/main/java/org/example/study_hr/StudyHrApplication.java
@@ -0,0 +1,13 @@
+package org.example.study_hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/study_hr/StudyHrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.study_hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
