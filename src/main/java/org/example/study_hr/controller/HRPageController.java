00808e7841c563700b13b89c0034cd252d7c87aa
frod <won1680@naver.com>
2024-08-14 16:54:03 +0900

    Feat: implement page move function

--- src/main/java/org/example/study_hr/controller/HRPageController.java
+++ src/main/java/org/example/study_hr/controller/HRPageController.java
@@ -1,13 +1,30 @@
 package org.example.study_hr.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.example.study_hr.service.HRService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class HRPageController {
 
-    private final HRService hrService;
+	private final HRService hrService;
+
+	@GetMapping("/search")
+	public String search() {
+		return "search.html";
+	}
+
+	@GetMapping("/insert")
+	public String insert() {
+		return "insert.html";
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "home.html";
+	}
 
 }
