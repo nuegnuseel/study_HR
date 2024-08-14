b596d5028a4f167f9da1ddc773accb6471626890
jin0815b <wlsgus25843@gmail.com>
2024-08-14 14:43:39 +0900

    Refactor : Service 구조

--- src/main/java/org/example/study_hr/service/EmployeeAttendanceService.java
+++ src/main/java/org/example/study_hr/service/EmployeeAttendanceService.java
@@ -2,12 +2,13 @@ package org.example.study_hr.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.study_hr.dao.ShareDAO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeAttendanceService {
-    private final SqlSessionTemplate sqlSessionTemplate;
+    private final ShareDAO shareDAO;
 
 }
--- src/main/java/org/example/study_hr/service/EmployeeService.java
+++ src/main/java/org/example/study_hr/service/EmployeeService.java
@@ -1,11 +1,12 @@
 package org.example.study_hr.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.study_hr.dao.ShareDAO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
-    private final SqlSessionTemplate sqlSessionTemplate;
+    private final ShareDAO shareDAO;
 }
